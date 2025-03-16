68a7fd1d24b2afaa04840ddd0f1591fb8d80e56e
Jeffrey Junior <51208117+JeffreyJuinior@users.noreply.github.com>
2025-03-17 06:48:37 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,8 @@ def main():
   st.info('This app for dhermatology classification')
 
   erythema = st.slider('Erythema', min_value = 0, max_value = 3, value = 2)
-  
+  scaling = st.slider('Scaling', min_value = 0, max_value = 3, value = 2)
+  definite_borders = st.slider('Definite Borders', min_value = 0, max_value = 3, value = 2)
   
 
 
