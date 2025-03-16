976ab51ccf766edaf35ba8b85a423b9c28899c95
Jeffrey Junior <51208117+JeffreyJuinior@users.noreply.github.com>
2025-03-17 06:46:17 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,12 @@ import streamlit as st
 def main():
 
   st.title('Machine Learning App')
-  
   st.info('This app for dhermatology classification')
+
+  erythema = st.slider('erythema', min_value = 0, max_value = 3, value = 2)
+  
+  
+
 
 if __name__=="__main__":
   main()
